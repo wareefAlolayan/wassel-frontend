8649901ef5bfe14e5ca0ffd417af6158563c9270
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-29 19:27:20 +0300

    complete styling for top bar

--- src/components/Manager/ManagerHome.jsx
+++ src/components/Manager/ManagerHome.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router'
 import './ManagerHome.css'
+import { MdLogout } from "react-icons/md" //react-icons website
 
 function ManagerHome() {
   const { managerId } = useParams()
@@ -29,15 +30,28 @@ function ManagerHome() {
 
   return (
     <div className='manager-home'>
-      <div className='bar-left'>
-        <img src='/src/components/assets/wassel_logo.png' alt='Wassel Logo' className='logo' />
-        <div className='bar-welcome'>
-          <p>Welcome ,</p>
-          <p>  {manager.name}  </p>
+      <div className='top-bar'>
+        <div className='bar-left'>
+          <img src='/src/components/assets/wassel_logo.png' alt='Wassel Logo' className='logo' />
+          <div className='bar-welcome'>
+            <p>Welcome ,</p>
+            <p id='manager-name'>{manager.name}</p>
+          </div>
+
+        </div>
+        <button className="logout-btn"><MdLogout />Logout</button>
+      </div>
+      <div className='home-body'>
+        <div className='nav-bar'>
+          here is nav bar
         </div>
 
+        <div className='body'>
+          here body
+        </div>
       </div>
-      <button className="logout-btn">Logout</button>
+
+
     </div>
   )
 }
