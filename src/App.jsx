2e42c7763da4c28e4d8492c8dabfbf80c72cdb92
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-29 13:17:23 +0300

    finish manager home bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TeamsList from './components/Manager/Teams/TeamsList'
 import TeamsForm from './components/Manager/Teams/TeamsForm'
 import Home from './components/Home'
 import EmployeeList from './components/Manager/Employees/EmployeeList'
+import ManagerHome from './components/Manager/ManagerHome'
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
 
-
+        <Route path='/manager/:managerId' element={<ManagerHome />} />
         <Route path='/manager/teams' element={<TeamsList />} />
         <Route path='/manager/teams/:teamId/edit' element={<TeamsForm/>}/>
         <Route path='/manager/employees' element={<EmployeeList/>}/>
--- src/components/Manager/ManagerHome.jsx
+++ src/components/Manager/ManagerHome.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router'
+import './ManagerHome.css'
 
 function ManagerHome() {
+  const { managerId } = useParams()
+  const [manager, setManager] = useState({})
+  const [errors, setErrors] = useState(null)
+
+  async function getManager() {
+    try {
+      const response = await axios.get(`http://127.0.0.1:8000/api/employees/${managerId}`)
+      console.log(response.data)
+      setManager(response.data)
+    } catch (error) {
+      console.log(error)
+      setErrors(error.response.data.error)
+    }
+  }
+
+  useEffect(() => {
+    getManager()
+  }, [])
+
+  if (errors) {
+    return <h3>{errors}</h3>
+  }
+
   return (
-    <div>ManagerHome</div>
+    <div className='manager-home'>
+      <div className='bar-left'>
+        <img src='/src/components/assets/wassel_logo.png' alt='Wassel Logo' className='logo' />
+        <div className='bar-welcome'>
+          <p>Welcome ,</p>
+          <p>  {manager.name}  </p>
+        </div>
+
+      </div>
+      <button className="logout-btn">Logout</button>
+    </div>
   )
 }
 
