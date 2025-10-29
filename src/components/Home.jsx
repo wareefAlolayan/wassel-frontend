3b355c9ded04132507fefc2e9ad6a9a97c67f923
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-29 12:04:20 +0300

    main home page with css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../App.css'
 
 function Home() {
   return (
-    <div>
-        this is the home page , will contain name logo descriptoon and a link to login page 
+    <div className='main-page'>
+      <div className='left-side'>
+        <div className='left-side-content'>
+          <img src='/src/components/assets/wassel_logo.png' alt='wassel logo' className='logo' />
+          <div className='text-content'>
+            <h1 className='website-name'>Wassel</h1>
+            <p className='tagline'>Connecting teams. Simplifying work</p>
+          </div>
+        </div>
+      </div>
+      <div className='right-side'>
+        login component will go here
+      </div>
     </div>
   )
 }
---
+++ src/components/Manager/ManagerHome.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ManagerHome() {
+  return (
+    <div>ManagerHome</div>
+  )
+}
+
+export default ManagerHome
+
